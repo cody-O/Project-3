929a4b8dede54d6cbc5c1ae7dee616cb132ea53f
codyoneil <74210654+codyoneil@users.noreply.github.com>
2024-11-16 19:53:32 -0500

    Everything now working except for the input

--- cars.c
+++ cars.c
@@ -1,57 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-
-    
-// Get inputs
-     
-
-
-
-    int ncols = 10;
-    int nrows = 3;
-    int speeds[] = {getSpeed('A'),getSpeed('A'),getSpeed('B'),getSpeed('B')};
-    int starts[] = {1,3,5,7};
-    int numberOfCars = sizeof(starts) / sizeof(starts[0]);
-
-
-    // Create Main Array [isCar, speed]
-    int *A = (int *) malloc(nrows*ncols*sizeof(int));
-    int **arr = (int **) malloc(nrows*sizeof(int *));
-    for (int i=0; i<nrows; i++){
-        arr[i] = A + i*ncols;
-    }
-
-    // Start assigning values
-    for (int i = 0; i<ncols; i++){
-        for (int j = 0; j<nrows; j++){
-            arr[i][j] = 0;
-        }
-    }
-    for (int i = 0; i<ncols; i++){
-        for (int j = 0; j<numberOfCars; i++){
-            if (i == starts[j]){
-                arr[i][0] = 1;
-                arr[i][1] = speeds[j];
-            }
-        }
-    }
-
-    // Run Sim
-    int response = runSimulation(arr, ncols, numberOfCars);
-
-    printf("%d", response);
-
-
-
-
-    return 0;
-
-}
-
 // Im sure theres a more elegant way to do this with taking the raw value of the letter but thats stupid so I brute force it
-int getSpeeds(char letter){
+int getSpeed(char letter){
     if (letter == 'A'){
         return 1;
     } else if (letter == 'B'){
@@ -75,32 +26,37 @@ int getSpeeds(char letter){
     }
 }
 
-int runSimulation(int **arr, int length, int bunches){
-    int bnchs = 0;
-    if (bunches == 0){
-        return -1;
+int jumpCheck(int **arr, int start, int speed){
+    int r = 0;
+    for (int i = 0; i<speed;i++){
+        if (arr[i+start+1][0]){
+            r = 1;
+        }
     }
-
-    for(int i=0; i<length;i++){
-        bnchs += step(arr, length);
-    }
-    return bnchs;
+    return r;
 }
 
 int step(int **arr, int length){
     int ret = 0;
+    int ilength = length-1;
     // Stepping backwards as to not overwrite bunches that havent moved yet
-    for(int i=length-1; i>-1;i--){
+    for(int i=ilength; i>-1;i--){
         if(arr[i][0]){
-            //"lock in" bunch if it reaches tghe end
-            if(i + arr[i][1] > length){
+            //"lock in" bunch if it reaches the end
+            if(i + arr[i][1] >= ilength){
                 ret++;
-            }
-            // If bunch doesnt reach end or doesnt catch up to another bunch
-            if(i + arr[i][1] < length || !arr[i+arr[i][1]][0]){
-                // Move Car
-                arr[i+arr[i][1]][0] = 1;
-                arr[i+arr[i][1]][1] = arr[i][1];
+            } else {
+                // If bunch doesnt reach end or doesnt catch up to another bunch
+                if(!jumpCheck(arr, i, arr[i][1])){
+                    // Move Car
+                    arr[i+arr[i][1]][0] = 1;
+                    arr[i+arr[i][1]][1] = arr[i][1];
+                    if(i + arr[i][1] < ilength){
+                        // Move Car
+                        arr[i+arr[i][1]][0] = 1;
+                        arr[i+arr[i][1]][1] = arr[i][1];
+                    }
+                } 
             }
             // Delete old bunch
             arr[i][0] = 0;
@@ -110,3 +66,75 @@ int step(int **arr, int length){
     }
     return ret;
 }
+
+int runSimulation(int **arr, int length, int bunches){
+    int bnchs = 0;
+    if (bunches == 0){
+        return -1;
+    }
+    printf("\n");
+    for(int i=0; i<length;i++){
+        for (int i=0; i<10; i++){
+            printf("%d", arr[i][0]);
+        }
+        printf("\n");
+        for (int i=0; i<10; i++){
+            printf("%d", arr[i][1]);
+        }
+        printf("\n");
+        printf("\n");
+        bnchs += step(arr, length);
+        
+        
+    }
+    return bnchs;
+}
+
+
+
+
+int main(void) {
+
+    
+// Get inputs
+    
+
+
+
+    int ncols = 10;
+    int nrows = 2;
+    int speeds[] = {getSpeed('D'),getSpeed('B'),getSpeed('A'),getSpeed('A')};
+    int starts[] = {0,1,4,7};
+    int numberOfCars = sizeof(starts) / sizeof(starts[0]);
+
+
+    // Create Main Array [isCar, speed]
+    int *A = (int *) malloc(nrows*ncols*sizeof(int));
+    int **arr = (int **) malloc(ncols*sizeof(int *));
+    for (int i=0; i<ncols; i++){
+        arr[i] = A + i*nrows;
+    }
+
+    // Start assigning values
+    for (int i = 0; i<ncols; i++){
+        for (int j = 0; j<nrows; j++){
+            arr[i][j] = 0;
+        }
+    }
+    for (int i = 0; i<ncols; i++){
+        for (int j = 0; j<numberOfCars; j++){
+            if (i == starts[j]){
+                arr[i][0] = 1;
+                arr[i][1] = speeds[j];
+            }
+        }
+    }
+    // Run Sim
+    int response = runSimulation(arr, ncols, numberOfCars);
+
+    printf("%d", response);
+    return 0;
+
+}
+
+
